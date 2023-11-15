ffdaaa4b1cbb58ef632a539bae949f150ca53584
timcol1 <avlyakulovtimur@gmail.com>
2023-11-15 18:26:12 +0200

    first commit

---
+++ src/test/java/team/challenge/MobileStore/MobileStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package team.challenge.MobileStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
