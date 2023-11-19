0b52ab9025058f9925b26e617a7cf0bb402928d1
timcol1 <avlyakulovtimur@gmail.com>
2023-11-19 12:36:04 +0200

    add cloud host connection

--- src/main/java/team/challenge/MobileStore/MobileStoreApplication.java
+++ src/main/java/team/challenge/MobileStore/MobileStoreApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MobileStoreApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(MobileStoreApplication.class, args);
 	}
-
 }
