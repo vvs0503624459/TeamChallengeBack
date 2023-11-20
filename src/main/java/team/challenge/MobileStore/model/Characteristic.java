ea5cd21ec21b8257d08f687c643e5d9a3f27885c
timcol1 <avlyakulovtimur@gmail.com>
2023-11-20 21:21:48 +0200

    add model device

---
+++ src/main/java/team/challenge/MobileStore/model/Characteristic.java
@@ -0,0 +1,10 @@
+package team.challenge.MobileStore.model;
+
+
+import lombok.Data;
+
+@Data
+public class Characteristic {
+    private String title;
+    private String value;
+}
---
+++ src/main/java/team/challenge/MobileStore/model/Device.java
@@ -0,0 +1,28 @@
+package team.challenge.MobileStore.model;
+
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+/**
+ * Model with information about device
+ **/
+
+
+@Document
+@Data
+public class Device {
+    @Id
+    private String id;
+    List<Characteristic> characteristics;
+    String uriMainPhoto;
+    List<String> uriPhotos;
+    Integer price;
+    Integer discount;
+    @DocumentReference
+    List<Review> reviews;
+}
