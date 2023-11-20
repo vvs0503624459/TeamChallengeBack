fd28f46eb4fdb551c6d19a75ae23f3421fc4bb53
timcol1 <avlyakulovtimur@gmail.com>
2023-11-20 21:31:42 +0200

    add two values for Characteristics

--- src/main/java/team/challenge/MobileStore/model/Characteristic.java
+++ src/main/java/team/challenge/MobileStore/model/Characteristic.java
@@ -5,7 +5,9 @@ import lombok.Data;
 
 @Data
 public class Characteristic {
-    //todo зробити розбиття на менші групи за Характерістік групи;
+
     private String title;
     private String value;
+    private String descriptionExtra;
+    private boolean isMain;
 }
