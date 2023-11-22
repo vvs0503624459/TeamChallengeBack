61cf2f39cec3eb220cecee3e74804460c6655b88
timcol1 <avlyakulovtimur@gmail.com>
2023-11-22 13:36:04 +0200

    add lombok for SpecificationGroup

--- src/main/java/team/challenge/MobileStore/model/SpecificationGroup.java
+++ src/main/java/team/challenge/MobileStore/model/SpecificationGroup.java
@@ -1,7 +1,10 @@
 package team.challenge.MobileStore.model;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class SpecificationGroup {
     String title;
     List<Specification> specifications;
