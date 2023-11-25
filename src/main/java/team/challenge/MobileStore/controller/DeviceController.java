f18a5919bdd5178fcea099b91f8ef3b3f0de6e08
timcol1 <avlyakulovtimur@gmail.com>
2023-11-25 18:02:24 +0200

    fill controller get all devices

--- src/main/java/team/challenge/MobileStore/controller/DeviceController.java
+++ src/main/java/team/challenge/MobileStore/controller/DeviceController.java
@@ -1,10 +1,16 @@
 package team.challenge.MobileStore.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import team.challenge.MobileStore.dto.mainPage.DeviceDtoShort;
 import team.challenge.MobileStore.model.Device;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Main controller to get all devices, create, update, delete
@@ -13,15 +19,41 @@ import java.util.List;
 @RequestMapping("/api/v1/devises")
 public class DeviceController {
 
+    DeviceService deviceService;
+
+    @Autowired
+    public DeviceController(DeviceService deviceService) {
+        this.deviceService = deviceService;
+    }
 
     @GetMapping
     public List<Device> getDevice() {
-
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(this.deviceService.findAll());
     }
 
     @PostMapping
-    public List<Device> addDevice(@RequestBody Device device) {
-
+    public List<Device> addDevice(
+            @RequestBody DeviceDto device,
+            UriComponentsBuilder uriComponentsBuilder) {
+        if (payload.details() == null || payload.details().isBlank()) {
+            final var message = this.messageSource
+                    .getMessage("tasks.create.details.errors.not_set",
+                            new Object[0], locale);
+            return ResponseEntity.badRequest()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(new ErrorsPresentation(
+                            List.of(message)));
+        } else {
+            var device = ;
+            this.taskRepository.save(task);
+            return ResponseEntity.created(uriComponentsBuilder
+                            .path("/api/tasks/{taskId}")
+                            .build(Map.of("taskId", task.id())))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(task);
+        }
     }
 
     @PutMapping("/{id}")
