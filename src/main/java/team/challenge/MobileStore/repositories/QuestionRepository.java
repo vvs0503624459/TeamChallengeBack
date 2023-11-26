f950c340391b06f90c650acf2e0149edc94aba33
Romych <lutsiv55@gmail.com>
2023-11-26 13:17:28 +0200

    Fix ReviewRepository and QuestionRepository.

--- src/main/java/team/challenge/MobileStore/repositories/QuestionRepository.java
+++ src/main/java/team/challenge/MobileStore/repositories/QuestionRepository.java
@@ -2,7 +2,8 @@ package team.challenge.MobileStore.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import team.challenge.MobileStore.model.Question;
 
 @Repository
-public interface QuestionRepository extends MongoRepository {
+public interface QuestionRepository extends MongoRepository<Question, String> {
 }
--- src/main/java/team/challenge/MobileStore/repositories/ReviewRepository.java
+++ src/main/java/team/challenge/MobileStore/repositories/ReviewRepository.java
@@ -2,6 +2,7 @@ package team.challenge.MobileStore.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import team.challenge.MobileStore.model.Review;
 
 @Repository
 public interface ReviewRepository extends MongoRepository<Review, String> {
