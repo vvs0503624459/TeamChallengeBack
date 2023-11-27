04bab23c9c77647ec7255a114036da86b83e8b3b
Romych <lutsiv55@gmail.com>
2023-11-27 21:58:34 +0200

    Update review and question repositories.

--- src/main/java/team/challenge/MobileStore/repositories/QuestionRepository.java
+++ src/main/java/team/challenge/MobileStore/repositories/QuestionRepository.java
@@ -1,9 +1,14 @@
 package team.challenge.MobileStore.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import team.challenge.MobileStore.model.Question;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository extends MongoRepository<Question, String> {
+    @Query("{'devices.id': ?0}")
+    List<Question> findAllByDeviceId(String deviceId);
 }
--- src/main/java/team/challenge/MobileStore/repositories/ReviewRepository.java
+++ src/main/java/team/challenge/MobileStore/repositories/ReviewRepository.java
@@ -1,10 +1,15 @@
 package team.challenge.MobileStore.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import team.challenge.MobileStore.model.Review;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends MongoRepository<Review, String> {
+    @Query("{'devices.id': ?0}")
+    List<Review> findAllByDeviceId(String deviceId);
 
 }
