96ed84a039c16df3d3e1bf04c6850bb4f80ebfbe
timcol1 <avlyakulovtimur@gmail.com>
2023-12-10 12:52:48 +0200

    change model of Catalogue

--- src/main/java/team/challenge/MobileStore/model/Catalogue.java
+++ src/main/java/team/challenge/MobileStore/model/Catalogue.java
@@ -1,18 +1,15 @@
 package team.challenge.MobileStore.model;
 
-import lombok.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
 @Document(collation = "catalogues")
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class Catalogue {
     /**
      * Model for catalogue in header
@@ -27,15 +24,8 @@ public class Catalogue {
 
 
     /**
-     * title - name of position in header catalogue
+     * catalogueGroups - name of position in header catalogue
      */
-    @Indexed(unique = true)
-    private String title;
-
-    /**
-     * groupSpecifications - groups of specifications for device in catalogue
-     */
-
-    private List<CatalogueGroupSpecification> groupSpecifications;
+    private List<CatalogueGroups> catalogueGroups;
 
 }
