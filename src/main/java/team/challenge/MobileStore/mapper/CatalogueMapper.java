b9a5a348e5591c63a4116ca164f278a532994476
timcol1 <avlyakulovtimur@gmail.com>
2023-12-11 14:05:46 +0200

    created catalogue mapper

--- src/main/java/team/challenge/MobileStore/mapper/CatalogueMapper.java
+++ src/main/java/team/challenge/MobileStore/mapper/CatalogueMapper.java
@@ -1,6 +1,12 @@
 package team.challenge.MobileStore.mapper;
 
+import team.challenge.MobileStore.dto.CatalogueRequest;
+import team.challenge.MobileStore.dto.CatalogueResponse;
+import team.challenge.MobileStore.model.Catalogue;
+
 public interface CatalogueMapper {
 
+    Catalogue mapCatalogueRequestToCatalogue(CatalogueRequest catalogueRequest);
 
+    CatalogueResponse mapCatalogueToCatalogueResponse(Catalogue catalogue);
 }
