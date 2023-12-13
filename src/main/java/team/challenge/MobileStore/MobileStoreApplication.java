9836fc7e4c758a921b81f9b4716d97bf2931885a
Romych <lutsiv55@gmail.com>
2023-12-13 13:45:59 +0200

    Enable mongock migration.

--- src/main/java/team/challenge/MobileStore/MobileStoreApplication.java
+++ src/main/java/team/challenge/MobileStore/MobileStoreApplication.java
@@ -1,9 +1,11 @@
 package team.challenge.MobileStore;
 
+import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableMongock
 public class MobileStoreApplication {
 
 
