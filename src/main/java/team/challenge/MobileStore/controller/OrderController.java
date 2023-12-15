650dc748f93c213b2d6a7b3d2f6da94bd74a2f28
timcol1 <avlyakulovtimur@gmail.com>
2023-12-15 12:52:44 +0200

    finish with order item controller

--- src/main/java/team/challenge/MobileStore/controller/OrderController.java
+++ src/main/java/team/challenge/MobileStore/controller/OrderController.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team.challenge.MobileStore.exception.ApiError;
+import team.challenge.MobileStore.model.Brand;
 import team.challenge.MobileStore.model.Customer;
 import team.challenge.MobileStore.model.Order;
 import team.challenge.MobileStore.service.OrderService;
@@ -23,7 +24,6 @@ public class OrderController {
 
     private OrderService orderService;
 
-    @GetMapping
     @Operation(summary = "Get all orders")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
@@ -40,11 +40,11 @@ public class OrderController {
                                     schema = @Schema(implementation = ApiError.class))
                     })
     })
+    @GetMapping
     public ResponseEntity<List<Order>> getAllOrders() {
         return ResponseEntity.ok(orderService.getAllOrders());
     }
 
-    @GetMapping("/{orderId}")
     @Operation(summary = "Get one order by id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
@@ -61,11 +61,11 @@ public class OrderController {
                                     schema = @Schema(implementation = ApiError.class))
                     })
     })
+    @GetMapping("/{orderId}")
     public ResponseEntity<Order> getOrderById(@PathVariable String orderId) {
         return ResponseEntity.ok(orderService.getOneOrderById(orderId));
     }
 
-    @PostMapping
     @Operation(summary = "Create one order")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
@@ -79,6 +79,7 @@ public class OrderController {
                                     schema = @Schema(implementation = ApiError.class))
                     })
     })
+    @PostMapping
     public ResponseEntity<Order> createOrder(
             @RequestBody Customer customer,
             @RequestParam String deliveryType,
@@ -88,7 +89,6 @@ public class OrderController {
         return ResponseEntity.ok(orderService.createOrder(customer, deliveryType, paymentType));
     }
 
-    @DeleteMapping("/{orderId}")
     @Operation(summary = "Delete one order by id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204",
@@ -102,11 +102,27 @@ public class OrderController {
                                     schema = @Schema(implementation = ApiError.class))
                     })
     })
+    @DeleteMapping("/{orderId}")
     public ResponseEntity<String> deleteOrder(@PathVariable String orderId) {
         orderService.deleteOrder(orderId);
         return ResponseEntity.ok("Order was deleted");
     }
 
+    @Operation(summary = "Update Order.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Updated order info.",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = Brand.class))
+                    }),
+            @ApiResponse(responseCode = "404",
+                    description = "Order with present ID not found!",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiError.class))
+                    })
+    })
     @PutMapping("/{orderId}")
     public ResponseEntity<Order> updateOrder(@PathVariable String orderId, @RequestBody Order updatedOrder) {
         return ResponseEntity.ok(orderService.updateOrder(orderId, updatedOrder));
--- src/main/java/team/challenge/MobileStore/controller/OrderItemController.java
+++ src/main/java/team/challenge/MobileStore/controller/OrderItemController.java
@@ -1,8 +1,14 @@
 package team.challenge.MobileStore.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import team.challenge.MobileStore.exception.ApiError;
 import team.challenge.MobileStore.model.OrderItem;
 import team.challenge.MobileStore.service.OrderItemService;
 
@@ -15,6 +21,19 @@ public class OrderItemController {
 
     private OrderItemService orderItemService;
 
+    @Operation(summary = "Add one order item to order")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Create one order item"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Order item wasn't created",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiError.class))
+                    }),
+    })
     @GetMapping
     public ResponseEntity<OrderItem> addOrderItem(
             Map<String, String> params
@@ -22,6 +41,19 @@ public class OrderItemController {
         return ResponseEntity.ok(orderItemService.addOrderItem(params));
     }
 
+    @Operation(summary = "Delete one order item by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    description = "Delete one order item by ID"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Order item with present ID not found!",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiError.class))
+                    })
+    })
     @DeleteMapping("/{orderItemId}")
     public ResponseEntity<?> deleteOrderItem(@PathVariable String orderItemId) {
         orderItemService.deleteOrderItem(orderItemId);
