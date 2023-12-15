d52da26d18aec9110e208df73a1bd5b676b65671
Romych <lutsiv55@gmail.com>
2023-12-15 20:58:57 +0200

    Create TokenFilter.

--- src/main/java/team/challenge/MobileStore/security/oauth/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ src/main/java/team/challenge/MobileStore/security/oauth/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -5,8 +5,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.stereotype.Component;
 import team.challenge.MobileStore.util.CookieUtil;
-
+@Component
 public class HttpCookieOAuth2AuthorizationRequestRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
     public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
     public static final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
